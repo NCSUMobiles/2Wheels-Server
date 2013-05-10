ef6cc6c236c8170eaf574382eee4cac899e1e60c
pshegde@ncsu.edu <pshegde@ncsu.edu>
2013-05-09 23:59:11 -0400

    changes for submitting details to the server

---
+++ war/script.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+			//alert("Welcome to Spinning Wellness!");
+			$('#log').hide();
+			
+			
+			$.ajax({
+				type: "GET",
+				url: "resources/l2wuser/getallusers",
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				success: function(response) {
+					for (var i = 0; i < response.user.length; i++) {
+						var aUsername = response.user[i].name;
+						var o = new Option(aUsername, aUsername);
+						/// jquerify the DOM object 'o' so we can use the html method
+						$(o).html(aUsername);
+						$("#userList").append(o);
+					}
+					
+					$("#userList").change(function(){
+					    var user = $("#userList").val() ;
+					    
+					    showmyupcomingrides(user);
+					});
+				
+			  },
+			failure: function(errMsg) {
+				alert("in error!");
+				$("#errMessage").text(errMsg);
+			}
+		});
+	});
+		
+function showmyupcomingrides(user){
+	var dateVal = new Date().getTime();
+	alert(dateVal);
+	$.ajax({
+		type: "GET",
+		url: "resources/l2wride/mypastrides/" + user + "/" + dateVal,
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function(response) {
+			if(response != null){
+				for (var i = 0; i < response.ride.length; i++) {
+					var aride = response.ride[i];
+					var o = new Option(aride.name, aride.id);
+					/// jquerify the DOM object 'o' so we can use the html method
+					$(o).html(aride.name);
+					$("#rideList").append(o);
+				}
+			} else{
+				 $("#errMessage").text("No rides found");
+			}
+			$("#rideList").change(function(){
+			    var rideid = $("#rideList").val() ;
+			    getRideDetails(rideid,user);
+			});
+		
+	},
+	failure: function(errMsg) {
+		alert("in error!");
+		$("#errMessage").text(errMsg);
+	}
+});	
+}	
+	
+function getRideDetails(ride,user){
+	alert("get ride details" + ride);
+	$('#log').show();
+	$("#submitButton").click({param1: ride, param2: user},submitDetail);
+}
+
+function submitDetail(event){
+	alert("submit clicked" + event.data.param1 + " " + event.data.param2);
+	
+	var currentState = {}; 
+	currentState["id"] = "050913234949851";
+	currentState["rideId"] = event.data.param1 ;
+	alert("gome");
+	currentState["userName"] = event.data.param2;
+	currentState["distaceCovered"] = $("#distance").val();
+	currentState["cadence"] = $("#cadence").val();
+	currentState["averageSpeed"] = $("#cal").val();
+	currentState["caloriesBurned"] = $("#time").val();
+	currentState["heartRate"] = $("#heartrate").val();
+	currentState["activityDate"] = new Date().getTime();
+	
+	alert(JSON.stringify(currentState));
+	$.ajax({
+		type: "POST",
+		url: "resources/l2wuser/loguseractivity" ,
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		data: JSON.stringify(currentState),
+		success: function(response) {
+			alert("details are logged");
+		},
+	failure: function(errMsg) {
+		alert("in error!");
+		$("#errMessage").text("Unable to log the details.");
+	}
+});	
+}
